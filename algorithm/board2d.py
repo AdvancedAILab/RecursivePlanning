7ac21c29808dd9623aadbe1838ffa018a4f122a5
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2019-08-01 19:02:33 +0900

    multi process Alpha Zero OK

--- algorithm/board2d.py
+++ algorithm/board2d.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 from .torch_module import *
 
-class Encoder(nn.Module):
+class Encoder(BaseNet):
     def __init__(self, env):
         super().__init__()
 
@@ -12,8 +12,8 @@ class Encoder(nn.Module):
         input_shape = state.feature().shape
         self.board_size = input_shape[1] * input_shape[2]
 
-        self.conv0 = Conv(input_shape[0], 16, 3, bn=True)
-        self.blocks = nn.ModuleList([WideResidual(16, 3, bn=True) for _ in range(4)])
+        self.conv = Conv(input_shape[0], 16, 3, bn=True)
+        self.blocks = nn.ModuleList([WideResidual(16, 3, bn=True) for _ in range(2)])
 
     def forward(self, x):
         h = F.relu(self.conv(x))
@@ -21,24 +21,37 @@ class Encoder(nn.Module):
             h = block(h)
         return h
 
-class Decoder(nn.Module):
-    def __init__(self, env, ):
+class Decoder(BaseNet):
+    def __init__(self, env):
         super().__init__()
 
         state = env.State()
         input_shape = state.feature().shape
         self.board_size = input_shape[1] * input_shape[2]
+        self.action_length = env.State().action_length()
 
-        self.conv_p = Conv(16, 2, 3, bn=True)
-        self.fc_p = nn.Linear(self.board_size * 2, env.State().action_length())
-        
-        self.conv_v = Conv(16, 1, 3, bn=True)
-        self.fc_v = nn.Linear(self.board_size * 1, 1)
-    
-    def forward(self, encoded_dep):
-        h = encoded_dep
+        self.conv_p = Conv(16, 2, 1, bn=True)
+        self.fc_p = nn.Linear(self.board_size * 2, self.action_length, bias=False)
+
+        self.conv_v = Conv(16, 1, 1, bn=True)
+        self.fc_v = nn.Linear(self.board_size * 1, 1, bias=False)
+
+    def forward(self, encoded):
+        h = encoded
+
         h_p = F.relu(self.conv_p(h))
+        h_p = self.fc_p(h_p.view(-1, self.board_size * 2))
+
         h_v = F.relu(self.conv_v(h))
-        h_p = self.fc_p(h_p).view(h_p.size(0), -1)
-        h_v = self.fc_v(h_v)
-        return F.softmax(h, dim=-1), torch.tanh(h_v)
+        h_v = self.fc_v(h_v.view(-1, self.board_size * 1))
+
+        return F.softmax(h_p, dim=-1), torch.tanh(h_v)
+
+class PVNet(BaseNet):
+    def __init__(self, env):
+        super().__init__()
+        self.encoder = Encoder(env)
+        self.decoder = Decoder(env)
+
+    def forward(self, x):
+        return self.decoder(self.encoder(x))
