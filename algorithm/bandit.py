963707b9186519c5d924bdf5023c714d1118f3d6
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2019-09-05 16:56:06 +0900

    Add algorithm/bandit.py

---
+++ algorithm/bandit.py
@@ -0,0 +1,38 @@
+
+# bandit algorithms
+
+import numpy as np
+
+def mask_p(p, mask):
+    p = (p + 1e-16) *mask
+    return p / p.sum() 
+
+def pucb(s, p):
+    q_sum_all, n_all = s.q_sum_all + s.v / 2, s.n_all + 1
+    q = (q_sum_all / n_all + s.q_sum) / (1 + s.n)
+    pucb = q + 2.0 * np.sqrt(n_all) * p / (s.n + 1) - s.action_mask
+    action = np.argmax(pucb)
+    return action, pucb
+
+def thompson(s, n_prior = 1):
+    q_sum_all, n_all = s.q_sum_all + s.v / 2 * n_prior, s.n_all + n_prior
+    q_sum, n = s.q_sum + q_sum_all / n_all * n_prior, s.n + n_prior # for n + n_prior games
+    q01_sum = (q_sum + n) / 2
+    alpha, beta = q01_sum, n - q01_sum + s.action_mask
+    r = np.random.beta(alpha, beta)
+    return np.argmax(r), r
+
+def pthompson(s, p):
+    if s.n_all == 0:
+        return np.random.choice(np.arange(len(p)), p=p), None
+    p_mod =p / np.max(s.p)
+    p_sum, ba = 0, None
+    for _ in range(16):
+        a, _ = thompson(s, 4)
+        r = np.random.random(2)
+        if r[0] < p_mod[a]:
+            return a, None
+        if r[1] * (p_sum + p_mod[a]) >= p_sum:
+            ba = a
+            p_sum += p_mod[a]
+    return ba, None
